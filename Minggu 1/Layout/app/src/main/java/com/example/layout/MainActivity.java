0a9a1814dfe4c103d01b92655bcc75d2b5dd4b6f
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-03-22 12:12:41 +0700

    menghapus activity dan mengubah constraint layout

--- Minggu 1/Layout/app/src/main/java/com/example/layout/MainActivity.java
+++ Minggu 1/Layout/app/src/main/java/com/example/layout/MainActivity.java
@@ -2,19 +2,14 @@ package com.example.layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constraint_layout);
 
     }
 }
