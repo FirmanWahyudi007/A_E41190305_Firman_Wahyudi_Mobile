b3c8536bf2d94702107e95de4baa4ac1a344ca92
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-03-24 07:40:35 +0700

    regis layout

--- Minggu 1/Layout/app/src/main/java/com/example/layout/MainActivity.java
+++ Minggu 1/Layout/app/src/main/java/com/example/layout/MainActivity.java
@@ -9,7 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint_layout);
+        setContentView(R.layout.registratrasi_layout);
 
     }
+
+    
 }
