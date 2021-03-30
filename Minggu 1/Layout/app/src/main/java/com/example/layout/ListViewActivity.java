4f3acd41ce1dc8a024a1894fe68029b74ed1eab9
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-03-30 14:53:01 +0700

    ListView Test

---
+++ Minggu 1/Layout/app/src/main/java/com/example/layout/ListViewActivity.java
@@ -0,0 +1,40 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.os.Bundle;
+
+public class ListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView listView;
+    ArrayAdapter<CharSequence> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        listView = (ListView)findViewById(R.id.listView);
+        adapter = ArrayAdapter.createFromResource(this,R.array.countries_array,android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.list, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this,adapter.getItem(position), Toast.LENGTH_SHORT).show();
+    }
+
+
+}
